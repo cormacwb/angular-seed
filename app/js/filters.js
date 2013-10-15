e48c0a4751c39144e8a1c1d9d59d34bf88edb919
Cormac Brennan <cormac.w.brennan@gmail.com>
2013-10-14 21:48:38 -0700

    Created a getImagePath filter along with content to test it on the partial view2.

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,18 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+  .filter('getImagePath', function(){
+    return function(input){
+      switch(input){
+      case 'running':
+        return "img/player_play.ico";
+      case 'stopped':
+        return "img/close.ico";
+      case 'done':
+        return "img/good_or_tick.ico";
+      default:
+        return "img/player_play.ico";
+      }
+    }
+  });
